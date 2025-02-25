100a69e6f3a0fd74f5f1973b2031009d58a18745
predefine <140549440+predefine@users.noreply.github.com>
2025-02-25 11:06:38 +0700

    OOPS

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
-ifneq ($(filter PMT3151_3G_3G,$(TARGET_DEVICE)),)
+ifneq ($(filter PMT3151_3G,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/prestigio/PMT3151_3G_3G
+LOCAL_PATH := device/prestigio/PMT3151_3G
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
